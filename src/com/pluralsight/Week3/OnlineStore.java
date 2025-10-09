1fc5aa019d48e593221a5c69b342151a72caed30
raspi <gitraspigner@gmail.com>
2025-10-09 08:54:34 -0700

    Add starter files for Workshop 3 (OnlineStore and Products)

---
+++ src/com/pluralsight/Week3/OnlineStore.java
@@ -0,0 +1,12 @@
+package com.pluralsight.Week3;
+
+/**
+ * *******Add program description here******
+ *
+ * @author Ravi Spigner
+ */
+public class OnlineStore {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/pluralsight/Week3/Product.java
@@ -0,0 +1,9 @@
+package com.pluralsight.Week3;
+
+/**
+ * *******Add program description here******
+ *
+ * @author Ravi Spigner
+ */
+public class Product {
+}
