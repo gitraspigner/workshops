c053e88298b11e0a9cd564f6113ccd0bb74d4731
raspi <gitraspigner@gmail.com>
2025-10-25 15:33:09 -0700

    Add Dealership implementation

---
+++ src/com/pluralsight/Week5/Dealership.java
@@ -0,0 +1,83 @@
+package com.pluralsight.Week5;
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * *******Add program description here******
+ *
+ * @author Ravi Spigner
+ */
+public class Dealership {
+    private String name;
+    private String address;
+    private String phone;
+    private final ArrayList<Vehicle> inventory;
+    public Dealership(String phone, String address, String name) {
+        this.phone = phone;
+        this.address = address;
+        this.name = name;
+        this.inventory = new ArrayList<Vehicle>();
+    }
+    public List<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+    public List<Vehicle> getVehiclesPriceRange(double upper, double lower) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getPrice() <= upper && v.getPrice() >= lower) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesYearRange(int upper, int lower) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getYear() <= upper && v.getYear() >= lower) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesMileageRange(int upper, int lower) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getOdometer() <= upper && v.getOdometer() >= lower) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesMakeOrModel(String makeOrModel) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getMake().equalsIgnoreCase(makeOrModel) || v.getModel().equalsIgnoreCase(makeOrModel)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesColor(String color) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getColor().equalsIgnoreCase(color)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public List<Vehicle> getVehiclesType(String type) {
+        List<Vehicle> result = new ArrayList<Vehicle>();
+        for(Vehicle v: inventory) {
+            if (v.getVehicleType().equalsIgnoreCase(type)) {
+                result.add(v);
+            }
+        }
+        return result;
+    }
+    public boolean addVehicle(Vehicle vehicle) {
+        return inventory.add(vehicle);
+    }
+    public boolean removeVehicle(Vehicle vehicle) {
+        return inventory.remove(vehicle);
+    }
+}
