e0aa54df88b164d7b00a18a72dea11e264b7a26d
raspi <gitraspigner@gmail.com>
2025-12-05 11:35:29 -0800

    Add Dealer class implementation

--- src/com/pluralsight/Week9/DealershipDealer.java
+++ src/com/pluralsight/Week9/DealershipDealer.java
@@ -1,9 +1,40 @@
 package com.pluralsight.Week9;
-
 /**
  * *******Add program description here******
  *
  * @author Ravi Spigner
  */
 public class DealershipDealer {
+    private String name;
+    private String address;
+    private String phone;
+    public DealershipDealer(String name, String address, String phone) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+    }
+    @Override
+    public String toString() {
+        return "name: " + name +
+                ", address: " + address +
+                ", phone: " + phone;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
 }
